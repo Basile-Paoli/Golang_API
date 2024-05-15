b7a14c1a0b3e0011325b03a2e623d176c11e6d55
Basile Paoli <basile.paoli@gmail.com>
2024-05-15 23:15:47 +0200

    add db functions

--- todo_repo.go
+++ todo_repo.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
 
 func dbConnection() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	dbName := os.Getenv("DB_NAME")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -18,14 +23,14 @@ func dbConnection() *sql.DB {
 	}
 	return db
 }
-func getTodosFromDB(todos *TodoList) {
+func getTodosFromDB(todos *[]Todo) {
 	db := dbConnection()
 	defer db.Close()
 	rows, err := db.Query("SELECT id,title,done FROM todos")
 	if err != nil {
 		log.Fatal(err)
 	}
-	*todos = TodoList{}
+	*todos = []Todo{}
 	for rows.Next() {
 		var todo Todo
 		err := rows.Scan(&todo.ID, &todo.Title, &todo.Done)
@@ -46,4 +51,23 @@ func postTodoToDB(todo Todo) int {
 		log.Fatal(err)
 	}
 	return id
+}
+func updateTodoInDB(todo Todo) Todo {
+	db := dbConnection()
+	defer db.Close()
+	row := db.QueryRow("UPDATE todos set done = $1, title=$2 where id = $3 returning id,title,done", todo.Done, todo.Title, todo.ID)
+	var res Todo
+	err := row.Scan(&res.ID, &res.Title, &res.Done)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
+func deleteTodoInDB(id int) {
+	db := dbConnection()
+	defer db.Close()
+	_, err := db.Exec("DELETE FROM todos WHERE id = $1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
